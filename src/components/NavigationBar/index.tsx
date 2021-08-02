12c28f2f8b7e47d395fb80ad2cbbefa582433e49
Xuejin <Xuejingao@gmail.com>
2021-08-02 00:09:23 -0400

    Added flex to navbar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -6,7 +6,6 @@ import PROFILE_IMG from "../../constant/images/xg_logo.jpeg";
 import styles from "./styles.module.css";
 
 const NavigationBar: React.FC = () => {
-  // console.log(styles);
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
