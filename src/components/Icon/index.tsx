d4bb095250ecb508778d7caf07b7234c8e5ac664
Xuejin <Xuejingao@gmail.com>
2021-08-06 18:37:05 -0400

    Added Icon Button

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+const Icon: React.FC = () => {
+  return <div className={styles.icon}>Icon</div>;
+};
+
+export default Icon;
