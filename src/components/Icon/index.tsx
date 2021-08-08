f659a5eb3e06581c0edb2881f1b8e8a1e4a459d3
Xuejin <Xuejingao@gmail.com>
2021-08-08 18:21:14 -0400

    Included the link factor

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,16 +2,29 @@ import React, { ReactElement } from "react";
 
 import styles from "./styles.module.css";
 
-const Icon: React.FC<{ size: number; children: ReactElement }> = ({
+import openInNewTab from "../../constant/helper/openInNewTab";
+
+export interface IconProps {
+  size: number;
+  link?: string;
+  children: ReactElement;
+}
+
+const Icon: React.FC<IconProps> = ({
   size,
+  link = "https://github.com/xuejingao",
   children,
 }) => {
   const styling = {
     fontSize: size,
   };
 
+  const handleClick = () => {
+    openInNewTab(link);
+  };
+
   return (
-    <div className={styles.icon} style={styling}>
+    <div className={styles.icon} style={styling} onClick={handleClick}>
       {children}
     </div>
   );
