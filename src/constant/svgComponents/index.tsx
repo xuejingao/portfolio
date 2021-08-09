df71a6f71dacde7be99799242b7bf6446a60f9b3
Xuejin <Xuejingao@gmail.com>
2021-08-09 02:21:35 -0400

    Added more language's icons

--- src/constant/svgComponents/index.tsx
+++ src/constant/svgComponents/index.tsx
@@ -6,6 +6,14 @@ import {
   SiMysql,
   SiLinkedin,
   SiGithub,
+  SiRedux,
+  SiJavascript,
+  SiCplusplus,
+  SiJava,
+  SiPython,
+  SiNodeDotJs,
+  SiTensorflow,
+  SiFlask,
 } from "react-icons/si";
 import { TiMail } from "react-icons/ti";
 import { ReactElement } from "react";
@@ -16,7 +24,15 @@ type STACK_OPTIONS = {
 
 export const TECH_STACK: STACK_OPTIONS = {
   typescript: <SiTypescript />,
+  javascript: <SiJavascript />,
+  cpp: <SiCplusplus />,
+  java: <SiJava />,
+  python: <SiPython />,
+  tensorflow: <SiTensorflow />,
+  flask: <SiFlask />,
   react: <SiReact />,
+  redux: <SiRedux />,
+  nodejs: <SiNodeDotJs />,
   css: <SiCss3 />,
   html: <SiHtml5 />,
   mysql: <SiMysql />,
