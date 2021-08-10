b9ea95357e1bb0172ce1d52076522ace56a85aee
Xuejin <Xuejingao@gmail.com>
2021-08-09 20:41:31 -0400

    Connected routes

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import PROFILE_IMG from "../../constant/images/xg_logo.jpeg";
 
-// const styles = require("./styles.css");
 import styles from "./styles.module.css";
 
-const NavigationBar: React.FC = () => {
+export type NavBarProps = {
+  landing?: boolean;
+  about?: boolean;
+  projects?: boolean;
+  blog?: boolean;
+};
+
+const NavigationBar: React.FC<NavBarProps> = ({
+  landing,
+  about,
+  projects,
+  blog,
+}) => {
+  const history = useHistory();
+
+  const landingButton = () => {
+    if (landing) history.push("/");
+  };
+  const aboutMeButton = () => {
+    if (about) history.push("/about");
+  };
+  const projectButton = () => {
+    if (projects) history.push("/projects");
+  };
+  const blogButton = () => {
+    if (blog) history.push("/blog");
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
@@ -13,12 +40,19 @@ const NavigationBar: React.FC = () => {
           className={styles.profile_img}
           src={PROFILE_IMG}
           alt="Xuejin Gao's Logo"
+          onClick={landingButton}
         />
 
         <ul className={styles.list}>
-          <li className={styles.listItem}>About Me</li>
-          <li className={styles.listItem}>Projects</li>
-          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem} onClick={aboutMeButton}>
+            About Me
+          </li>
+          <li className={styles.listItem} onClick={projectButton}>
+            Projects
+          </li>
+          <li className={styles.listItem} onClick={blogButton}>
+            Blog
+          </li>
           <li className={styles.listItem}>Contact</li>
         </ul>
       </div>
