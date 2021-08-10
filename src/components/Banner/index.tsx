2b59f37ffd2b05d679a7d6a2d6c8a0b8cfd552ab
Xuejin <Xuejingao@gmail.com>
2021-08-10 14:14:40 -0400

    Centered layout and added title attribute

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,38 +6,51 @@ import Icon from "../Icon";
 
 import { CONTACT_STACK } from "../../constant/svgComponents";
 
+import PROFILE_IMAGE from "../../constant/images/matteblackkb.jpeg";
+
 const LOGO_SIZE = 30;
 
-const Banner: React.FC = () => {
+export type BannerProps = {
+  displayImage?: boolean;
+  displayIcons?: boolean;
+  title?: string;
+  description?: string;
+};
+
+const Banner: React.FC<BannerProps> = ({
+  displayImage,
+  displayIcons,
+  title = "Xuejin Gao",
+  description = `About me Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quos quibusdam provident, ea ipsa culpa, eum, itaque deleniti cum repellendus architecto ducimus possimus voluptas omnis porro nesciunt nostrum ut explicabo?`,
+}) => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        {/* <img
-          className={styles.profile_pic}
-          src={PROFILE_IMAGE}
-          alt="PROFILE OF XUEJIN GAO"
-        /> */}
+        {displayImage ? (
+          <img
+            className={styles.profile_pic}
+            src={PROFILE_IMAGE}
+            alt="PROFILE OF XUEJIN GAO"
+          />
+        ) : null}
         <div className={styles.bioContainer}>
           <div className={styles.bio}>
-            <h3 className={styles.name}>Xuejin Gao</h3>
-            <p className={styles.description}>
-              About me Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Adipisci quos quibusdam provident, ea ipsa culpa, eum, itaque
-              deleniti cum repellendus architecto ducimus possimus voluptas
-              omnis porro nesciunt nostrum ut explicabo?
-            </p>
+            <h3 className={styles.name}>{title}</h3>
+            <p className={styles.description}>{description}</p>
 
-            <div className={styles.linkContainer}>
-              <Icon size={LOGO_SIZE} link="https://github.com/xuejingao">
-                {CONTACT_STACK["github"]}
-              </Icon>
-              <Icon size={LOGO_SIZE} link="https://linkedin.com/in/xuejingao">
-                {CONTACT_STACK["linkedin"]}
-              </Icon>
-              <Icon size={LOGO_SIZE} link="mailto:gxuejin13@gmail.com">
-                {CONTACT_STACK["mail"]}
-              </Icon>
-            </div>
+            {displayIcons ? (
+              <div className={styles.linkContainer}>
+                <Icon size={LOGO_SIZE} link="https://github.com/xuejingao">
+                  {CONTACT_STACK["github"]}
+                </Icon>
+                <Icon size={LOGO_SIZE} link="https://linkedin.com/in/xuejingao">
+                  {CONTACT_STACK["linkedin"]}
+                </Icon>
+                <Icon size={LOGO_SIZE} link="mailto:gxuejin13@gmail.com">
+                  {CONTACT_STACK["mail"]}
+                </Icon>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
