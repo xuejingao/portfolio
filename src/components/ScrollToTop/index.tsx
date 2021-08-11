132035e244f1151248d040a550e55b4dfcee25ae
Xuejin <Xuejingao@gmail.com>
2021-08-11 15:32:06 -0400

    Added scroll to top

---
+++ src/components/ScrollToTop/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+
+// import SCROLL_TOP_IMG from "../../constant/images/toparrow.png";
+
+import { DESIGN_STACK } from "../../constant/svgComponents";
+
+import styles from "./style.module.css";
+
+const ScrollToTop: React.FC = () => {
+  const [visiable, setVisibility] = useState(false);
+
+  const handleClick = () => {
+    scrollTop();
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setVisibility(true);
+    } else {
+      setVisibility(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <div className={styles.scrollTop}>
+      {visiable && (
+        <div className={styles.arrow} onClick={handleClick}>
+          {DESIGN_STACK["arrowUp"]}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
