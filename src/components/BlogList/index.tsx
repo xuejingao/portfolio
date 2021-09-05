2f9d6d7c0961c4d1e7797db180b88a52e797e61e
Xuejin Gao <xuejingao@gmail.com>
2021-09-05 00:05:27 -0400

    Added styling and include item

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -2,9 +2,15 @@ import React from "react";
 
 import styles from "./style.module.css";
 
+import BlogItem from "../BlogItem";
+
 const BlogList: React.FC = () => {
   return (
-    <div className={styles.container}>The blog page is under contruction!</div>
+    <div className={styles.container}>
+      <BlogItem />
+      <BlogItem />
+      The blog page is under contruction!
+    </div>
   );
 };
 
