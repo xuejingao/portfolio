477ab065fccbb658bb0c2729ce011f9e9ee8027a
Xuejin <Xuejingao@gmail.com>
2021-09-08 14:23:09 -0400

    Added icon hover

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -11,53 +11,61 @@ import PROFILE_IMAGE from "../../constant/images/promprofile.jpg";
 const LOGO_SIZE = 30;
 
 export type BannerProps = {
-  displayImage?: boolean;
-  displayIcons?: boolean;
-  title?: string;
-  description?: string;
+	displayImage?: boolean;
+	displayIcons?: boolean;
+	title?: string;
+	description?: string;
 };
 
 const Banner: React.FC<BannerProps> = ({
-  displayImage,
-  displayIcons,
-  title = "",
-  description = `About me Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quos quibusdam provident, ea ipsa culpa, eum, itaque deleniti cum repellendus architecto ducimus possimus voluptas omnis porro nesciunt nostrum ut explicabo?`,
+	displayImage,
+	displayIcons,
+	title = "",
+	description = `About me Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quos quibusdam provident, ea ipsa culpa, eum, itaque deleniti cum repellendus architecto ducimus possimus voluptas omnis porro nesciunt nostrum ut explicabo?`,
 }) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        {displayImage ? (
-          <img
-            className={styles.profile_pic}
-            src={PROFILE_IMAGE}
-            alt="PROFILE OF XUEJIN GAO"
-          />
-        ) : null}
-        <div className={styles.bioContainer}>
-          <div className={styles.bio}>
-            <h3 className={styles.name}>{title}</h3>
-            <p className={styles.description}>{description}</p>
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>
+				{displayImage ? (
+					<img
+						className={styles.profile_pic}
+						src={PROFILE_IMAGE}
+						alt="PROFILE OF XUEJIN GAO"
+					/>
+				) : null}
+				<div className={styles.bioContainer}>
+					<div className={styles.bio}>
+						<h3 className={styles.name}>{title}</h3>
+						<p className={styles.description}>{description}</p>
 
-            {displayIcons ? (
-              <div className={styles.linkContainer}>
-                <Icon size={LOGO_SIZE} link="https://github.com/xuejingao">
-                  {CONTACT_STACK["github"]}
-                </Icon>
-                <Icon size={LOGO_SIZE} link="https://linkedin.com/in/xuejingao">
-                  {CONTACT_STACK["linkedin"]}
-                </Icon>
-                <Icon size={LOGO_SIZE} link="mailto:gxuejin13@gmail.com">
-                  {CONTACT_STACK["mail"]}
-                </Icon>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
+						{displayIcons ? (
+							<div className={styles.linkContainer}>
+								<Icon
+									size={LOGO_SIZE}
+									link="https://github.com/xuejingao"
+									hover
+								>
+									{CONTACT_STACK["github"]}
+								</Icon>
+								<Icon
+									size={LOGO_SIZE}
+									link="https://linkedin.com/in/xuejingao"
+									hover
+								>
+									{CONTACT_STACK["linkedin"]}
+								</Icon>
+								<Icon size={LOGO_SIZE} link="mailto:gxuejin13@gmail.com" hover>
+									{CONTACT_STACK["mail"]}
+								</Icon>
+							</div>
+						) : null}
+					</div>
+				</div>
+			</div>
 
-      {/* <div>Banner</div> */}
-    </div>
-  );
+			{/* <div>Banner</div> */}
+		</div>
+	);
 };
 
 export default Banner;
